7bc8561491012083259353c1effdd8f0785e7e83
Lizardo <lizardo@utp.edu.co>
2021-09-10 23:03:46 -0500

    Entorno base para el backend del proyecto Quetz

---
+++ Quetz/Quetz/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+
+from django.contrib import admin
+
+admin.autodiscover()
+
+import api.views
+
+# To add a new path, first import the app:
+# import blog
+#
+# Then add the new path:
+# path('blog/', blog.urls, name="blog")
+#
+# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+urlpatterns = [
+    path("", api.views.index, name="index"),
+    path("db/", api.views.db, name="db"),
+    path("admin/", admin.site.urls),
+]
