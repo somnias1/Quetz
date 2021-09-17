ef8e75cd1c63a289a58d8b2c6c56027976866485
Lizardo <lizardo@utp.edu.co>
2021-09-16 20:52:30 -0500

    Segunda corrección Postgres

--- Quetz/Quetz/settings.py
+++ Quetz/Quetz/settings.py
@@ -91,20 +91,20 @@ WSGI_APPLICATION = "Quetz.wsgi.application"
 
 import dj_database_url
 
-DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
-"""DATABASES = {
+DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        """'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['QuetzDB'],
         'USER': os.environ['POSTGRES_USER'],
         'PASSWORD': os.environ['POSTGRES_PASSWORD'],
         'HOST': os.environ['POSTGRES_HOST'],
         'PORT': os.environ['POSTGRES_PORT'],
         #"ENGINE": "django.db.backends.sqlite3",
-        #"NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        #"NAME": os.path.join(BASE_DIR, "db.sqlite3"),"""
     }
-}"""
+}
+
+DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
